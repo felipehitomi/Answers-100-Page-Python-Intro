e2909a6e501494201318dadedb7ee51ba23af35c
felipe hitomi <felipe.hitomi@gmail.com>
2022-03-03 22:24:45 -0300

    More code

--- books/100PagesPythonIntro/exercises/controlStructures/5.py
+++ books/100PagesPythonIntro/exercises/controlStructures/5.py
@@ -6,3 +6,29 @@ Write a function that returns the maximum nested depth of curly braces for a giv
 a = '{{a+2}*{{b+{c*d}}+e*d}}'
 b = '{a}*b{'
 c = '}a+b{'
+
+def maxDepth(S):
+    current_max = 0
+    max = 0
+    n = len(S)
+
+    for i in range(n):
+        if S[i] == '{':
+            current_max += 1
+
+            if current_max > max:
+                max = current_max
+        elif S[i] == '}':
+            if current_max > 0:
+                current_max -= 1
+            else:
+                return -1
+
+    if current_max != 0:
+        return -1
+
+    return max
+
+print(maxDepth(a))
+print(maxDepth(b))
+print(maxDepth(c))
